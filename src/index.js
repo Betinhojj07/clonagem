e033bf9ea1f952ae1319dd554046666703a15d85
Roberto <betinho2007@gmail.com>
2024-07-28 13:44:01 -0300

    testes

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages';
-import Bala from './pages/idex2';
+import Bala from './pages/pagina2/idex2'
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
     <Bala/>
+    <App />
   </React.StrictMode>
 );
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,6 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello word</h1>
+
+      <a href="pages/pagina2">clique aqui</a>
+      <img src="/assets/img/icones.jpg"/>
     </div>
   );
 }
